98be1a6f212e47ae188134ce33c18f639a7f3d39
platinumwireless <shaimaa@sulfah.com>
2024-03-30 00:15:09 +0200

    Initial Commit

---
+++ SwiftUi-Weather/SwiftUi_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_WeatherApp.swift
+//  SwiftUi-Weather
+//
+//  Created by Platinum WireLess on 30/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
